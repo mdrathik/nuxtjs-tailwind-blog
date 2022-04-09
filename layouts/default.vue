bee36255387f53962016b98fe76b704f59fb9419
Md Rathik <hello@rathik.dev>
2022-04-09 16:12:08 +0600

    removed metadata completly

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,19 +4,19 @@
     <div class="mx-auto max-w-8xl">
       <TheHeader />
       <Nuxt />
-      <TheFooter />
+      <!-- <TheFooter /> -->
     </div>
   </section>
 </template>
 
 <script>
 import TheHeader from "../components/TheHeader.vue";
-import TheFooter from "../components/TheFooter.vue";
+/* import TheFooter from "../components/TheFooter.vue"; */
 
 export default {
   component: {
     TheHeader,
-    TheFooter,
+    // TheFooter,
   },
 };
 </script>
